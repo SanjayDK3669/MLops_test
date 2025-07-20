9a09eea67cfb11fc8ebc918bd6199c9c0d086aa4
SanjayDK3669 <dksanjay39@gmail.com>
2025-07-21 00:55:49 +0530

    File strucuture updated

--- templete.py
+++ templete.py
@@ -30,12 +30,12 @@ list_of_files = [
 
 
 for filepath in list_of_files:
-    filepath = Path(l=filepath)
+    filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
     
     if filedir != "":
         os.makedirs(filedir, exist_ok= True)
-        logging.info(f"Creating directory: {filedir} for file : {filename}")
+        #logging.info(f"Creating directory: {filedir} for file : {filename}")
         
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
